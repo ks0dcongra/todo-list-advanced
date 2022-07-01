6c98cfa2e9d7aeb81abe5765b16af64fc3576df2
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-01 14:33:12 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,13 +22,21 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
 
-// 載入設定檔，要寫在 express-session 以後
-const usePassport = require('./config/passport')
-// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-usePassport(app)
