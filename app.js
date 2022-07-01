2024c954c9dd2a41a577a3d85444b99721ccbe9e
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-01 19:39:56 +0800

    modify todoSeeder

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 require('./config/mongoose')
-
+// require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-console.log(PORT)
+// console.log(PORT)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -28,7 +33,6 @@ app.use(methodOverride('_method'))
 usePassport(app)
 app.use(flash())
 app.use((req, res, next) => {
-  // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
