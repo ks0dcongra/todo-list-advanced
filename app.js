03e18aed28644ecefb986ba237c7647582edda11
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-01 15:58:47 +0800

    feat: add relations between User and Todo models

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const session = require('express-session')
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
-// 載入設定檔，要寫在 express-session 以後
-const usePassport = require('./config/passport')
+console.log(PORT)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  // 引用 passport
-  const passport = require('passport')
-  // 加入 middleware，驗證 request 登入狀態
-  router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/users/login'
-  }))
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: 'users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
